71db4a2c9cb16db71356ad43f17f91c6cc2cea0e
Oleksandr Romaniuk <166243339+romaniuksashko@users.noreply.github.com>
2025-12-14 15:25:01 +0200

    Clean up main.js by removing unused lines
    Removed unnecessary blank lines and commented-out code.

--- main.js
+++ main.js
@@ -9,26 +9,22 @@ const intervalId = setInterval(() => {
   const dateNow = new Date();
   const time = targetDate - dateNow;
 
-
-
   const days = String(Math.floor(time / (1000 * 60 * 60 * 24))).padStart(2, "0");
   const hours = String(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, "0");
   const mins = String(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
   const secs = String(Math.floor((time % (1000 * 60)) / 1000)).padStart(2, "0");
-  // console.log(days, hours, mins, secs);
   
   daysRef.textContent = days;
   hoursRef.textContent = hours;
   minsRef.textContent = mins;
   secsRef.textContent = secs;
 
-    if (time <= 0) {
+  if (time <= 0) {
     clearInterval(intervalId);
     daysRef.textContent = "00";
     hoursRef.textContent = "00";
     minsRef.textContent = "00";
     secsRef.textContent = "00";
-    // document.body.style.backgroundColor = ;
   }  
 
 }, 1000)
